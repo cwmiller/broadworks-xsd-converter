d01a9dfeec4f872c35429328efc4e56872e8d994
Chase Miller <chase@miller.sh>
2017-12-28 21:57:26 -0500

    Added support for the debug flag

--- bin/convert.php
+++ bin/convert.php
@@ -6,16 +6,28 @@ use CWM\BroadWorksXsdConverter\Writer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if ($argc < 4) {
-    echo sprintf('Usage: %s path-to-root-xsd output-directory root-namespace', basename($argv[0])) . PHP_EOL;
+$args = $argv;
+$opts = [];
+
+$args = array_values(array_filter($argv, function($arg) {
+    return $arg[0] !== '-';
+}));
+
+$opts = array_values(array_filter($argv, function($arg) {
+    return $arg[0] === '-';
+}));
+
+if (count($args) < 4) {
+    echo sprintf('Usage: %s [-d] path-to-root-xsd output-directory root-namespace', basename($args[0])) . PHP_EOL;
     exit(-1);
 }
 
-$rootXsd = $argv[1];
-$outputDirectory = $argv[2];
-$rootNamespace = $argv[3];
+$rootXsd = $args[1];
+$outputDirectory = $args[2];
+$rootNamespace = $args[3];
+$debug = in_array('-d', $opts, true);
 
-$parser = new Parser($rootXsd, false);
+$parser = new Parser($rootXsd, $debug);
 $types = $parser->parse();
 
 $writer = new Writer($outputDirectory, $rootNamespace);
