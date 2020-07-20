8e78f1893d83c5ec10b84cecac8feb08442bbb45
Chase Miller <chase@miller.sh>
2020-07-19 22:25:25 -0400

    C# Async support

--- src/CS/templates/extension.cs.php
+++ src/CS/templates/extension.cs.php
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 <?php /** @var \CWM\BroadWorksXsdConverter\CS\ExtensionTemplate $template */
 foreach (array_unique($template->getUsings()) as $using) { ?>
@@ -15,10 +17,17 @@ public static class <?= $template->getName() ?>
     /// <summary>
     /// <?php echo implode("\n    /// ", array_map('trim', explode("\n", $method->getDocumentation()))) . PHP_EOL ?>
     /// </summary>
+    [Obsolete("This method is deprecated. Use <?= $method->getName() ?>Async instead.")]
     public static async Task<<?= $method->getReturnType() ?>> <?= $method->getName() ?>(this OcipClient client, <?= $method->getParamType() ?> request) {
-        return await client.Call(request) as <?= $method->getReturnType() ?>;
+        return await client.CallAsync(request).ConfigureAwait(false) as <?= $method->getReturnType() ?>;
     }
 
+    /// <summary>
+    /// <?php echo implode("\n    /// ", array_map('trim', explode("\n", $method->getDocumentation()))) . PHP_EOL ?>
+    /// </summary>
+    public static async Task<<?= $method->getReturnType() ?>> <?= $method->getName() ?>Async(this OcipClient client, <?= $method->getParamType() ?> request, CancellationToken cancellationToken = default) {
+        return await client.CallAsync(request, cancellationToken).ConfigureAwait(false) as <?= $method->getReturnType() ?>;
+    }
 <?php } ?>
 
 }
